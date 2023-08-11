fff68dcef97a27cbdc0f0fbaf35a265cb6f2f081
Naadu23 <Naadu23>
2023-08-11 04:33:20 -0700

    variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+i#include <stdio.h>
+
+/**
+ * main - Using putchar, prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
